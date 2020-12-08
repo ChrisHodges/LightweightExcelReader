6dab4eb96fa4b9fc7f48c2e81833e1dbd69146d9
Chris Hodges <chris@chrishodges.me.uk>
2020-12-08 18:07:26 +0000

    Ran benchmarks
    |                 Method |      Mean |     Error |    StdDev |
    |----------------------- |----------:|----------:|----------:|
    | LightweightExcelReader |  4.188 ms | 0.0451 ms | 0.0421 ms |
    |                OpenXml | 21.757 ms | 0.3339 ms | 0.2960 ms |
    |             KoograTest |        NA |        NA |        NA |
    |        ExcelDataReader |  9.504 ms | 0.0783 ms | 0.0694 ms |
    |       OleDbDataAdapter |        NA |        NA |        NA |

--- LightweightExcelReader.Benchmarks/Program.cs
+++ LightweightExcelReader.Benchmarks/Program.cs
@@ -6,13 +6,6 @@ namespace LightweightExcelReader.Benchmarks
     {
         private static void Main(string[] args)
         {
-            /*var benchmarks = new Benchmarks();
-            //benchmarks.NPoi();
-            benchmarks.OpenXml();
-            benchmarks.ExcelDataReader();
-            benchmarks.LightweightExcelReader();
-            //benchmarks.GemBox();*/
-
             BenchmarkRunner.Run<Benchmarks>();
         }
     }
